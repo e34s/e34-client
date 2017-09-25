ed572627d37514f186f06c030e9c96fb19efd7cd
freynaud <francois.reynaud@gmail.com>
2017-09-25 14:00:56 +0200

    disable live per default

--- client-common/src/main/java/com/element34/E34Settings.java
+++ client-common/src/main/java/com/element34/E34Settings.java
@@ -3,7 +3,7 @@ package com.element34;
 public class E34Settings {
 
   public static final String DATA = "data";
-  public final static boolean ENABLE_LIVE = true;
+  public final static boolean ENABLE_LIVE = false;
   public final static int E34_PORT = 8081;
   public final static String SCREENSHOTS_FOLDER = "screenshots";
 }
